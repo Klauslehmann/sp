8c2da7df073a2e265f266d20cfc37286c5e351bd
Klaus Lehmann <klehmann@fen.uchile.cl>
2022-12-23 12:05:25 +0000

    avance en script deployment

--- cabi_env/deploy.sh
+++ cabi_env/deploy.sh
@@ -15,10 +15,26 @@ docker build -t cabify_app .
 
 # Borrar el contenedor de shinyproxy y  echarlo a andar 
 docker rm -f shinyproxy
-docker run -v /var/run/docker.sock:/var/run/docker.sock:ro --group-add $(getent group docker | cut -d: -f3) --net sp-example-net --env-file /home/klaus/sp/cabi_env/.Renviron  --name shinyproxy   -p 8080:8080 shinyproxy 
+docker run -v /var/run/docker.sock:/var/run/docker.sock:ro --group-add $(getent group docker | cut -d: -f3) --net sp-example-net --env-file /home/klaus/sp/cabi_env/.Renviron  --name shinyproxy -p 8080:8080 shinyproxy 
 
 elif [$1 == "testing"]
 then 
+
+# Generar variable de ambiente
+echo -e "R_CONFIG_ACTIVE=$1" > .Renviron
+
+# Ir a la carpeta que tiene la aplicacion
+cd /home/prototipo_turnos
+pwd
+
+# Hacer pull desde el repo
+
+# Construir la imagen de la aplicación
+docker build -t cabify_app .
+
+# Borrar el contenedor de shinyproxy y  echarlo a andar 
+docker rm -f shinyproxy
+docker run -v /var/run/docker.sock:/var/run/docker.sock:ro --group-add $(getent group docker | cut -d: -f3) --net sp-example-net --env-file /home/sp/cabi_env/.Renviron  --name shinyproxy -p 9090:8080 shinyproxy 
 
 
 elif [$1 == "production"]
